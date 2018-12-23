bb0164e1bde69254d37cdcb191f5cb1b15b03f20
skanehira <sho19921005@gmail.com>
2018-12-23 21:22:14 +0900

    first commit

---
+++ src/content_script.js
@@ -0,0 +1,28 @@
+﻿//----------------------
+$(function () {
+    makeUho = function(val) {
+        let result = ""
+        for (i in val) {
+            if (val[i] == "\n") {
+                result += val[i]
+                continue
+            }
+            if (i % 2 == 0) {
+                result += "ｳ"
+            } else if (i % 5 == 0) {
+                result += "ﾎ"
+            } else if (i % 6 == 0) {
+                result += "ｯ"
+            } else {
+                result += "ｳﾎｯ"
+            }
+        }
+        return result
+    }
+
+    // my account 
+    var user_name = document.getElementsByClassName('u-textInheritColor js-nav')[0];
+    user_name.text = makeUho(user_name.text);
+});
+
+
